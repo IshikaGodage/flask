a3f4f49fd3458ea12ce500a415e11702daf78c65
Ishika Godage <ishigodage@gmail.com>
2024-06-04 18:19:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask 
 import os
+import db
+import recipes
+from dotenv import load_dotenv
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
+test = load_dotenv()  # take environment variables from .env.
+db.init_app(app)
+app.register_blueprint(recipes.bp)
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
