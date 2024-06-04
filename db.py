85f84c504f5bc800fbb4c6d43829e2a650975bbd
Ishika Godage <ishigodage@gmail.com>
2024-06-04 18:19:43 +0530

    Create db.py

---
+++ db.py
@@ -0,0 +1,31 @@
+import click
+import mysql.connector
+import os
+
+from flask import current_app, g
+from flask.cli import with_appcontext
+
+def get_db():
+    if "db" not in g or not g.db.is_connected():
+        # connect to the database
+        
+        g.db = mysql.connector.connect(
+            host=os.environ["DB_HOST"],
+            user=os.environ["DB_USERNAME"],
+            passwd=os.environ["DB_PASSWORD"],
+            db=os.environ["DB_DATABASE"],
+            port=os.environ["DB_PORT"]
+        )
+        
+    return g.db
+
+def close_db(e=None):
+    db = g.pop("db", None)
+
+    if db is not None:
+        # close the database 
+        db.close()
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    
