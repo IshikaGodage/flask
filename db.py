7b52b6acdbd59477ab55241826e91d13c0b485ce
Ishika Godage <ishigodage@gmail.com>
2024-06-04 20:32:09 +0530

    Update db.py

--- db.py
+++ db.py
@@ -13,8 +13,7 @@ def get_db():
             host=os.environ["DB_HOST"],
             user=os.environ["DB_USERNAME"],
             passwd=os.environ["DB_PASSWORD"],
-            db=os.environ["DB_DATABASE"],
-            port=os.environ["DB_PORT"]
+            db=os.environ["DB_DATABASE"]
         )
         
     return g.db
